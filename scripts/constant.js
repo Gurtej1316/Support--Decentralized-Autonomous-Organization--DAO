06af8524daadde4f8041d9cab2c66ef1b3b135f1
unknown <gurtej1316@gmail.com>
2022-07-31 13:05:33 +0530

    first commit

---
+++ scripts/constant.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x887f54981890A9796672EaA11af4aA9641901262";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
